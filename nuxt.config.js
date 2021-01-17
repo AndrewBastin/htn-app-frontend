c0572feb9f7dde117c52f87be42ba3a78a73c90e
Andrew Bastin <andrewbastin.k@gmail.com>
2021-01-16 22:42:28 -0500

    Add metas

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'htn',
+    title: 'COVIDgo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Are you planning to travel ? Use covidgo.online to calculate your risk factor of being exposed to the virus.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
