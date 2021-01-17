d846fa6498c2b67f1936f7245de638127a7ba32b
Andrew Bastin <andrewbastin.k@gmail.com>
2021-01-17 08:47:34 -0500

    Fill about page content

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>
+  <div class="flex flex-col h-screen">
     <Header selected="about" />
+      <div class="h-screen bg-purple-300 px-10 py-10">
+        <h1
+          class="text-3xl font-bold text-purple-800 mb-10"
+          >About</h1>
+        <p>
+        In these trying times of the pandemic, COVIDgo wants to help you be informed and help you make
+        travel decisions so that you can stay safe as much as possible and assess your risks wisely.
+        Using the data publicly available from the Ontario Health authorities and analyzing trends and happenings.
+        We give you a score of how much you will be exposing yourself if you carry out your plans for travel.
+
+        <br />
+        <br />
+        <br />
+        This website is built in 72 hours for <a
+                                                class="italic underline font-semibold" href="https://hackthenorth.com/">
+          Hack The North 2020++</a> by:
+        <ul class="list-disc px-10 py-3">
+          <li class="my-2">
+            <a class="font-semibold italic underline" href="https://andrewbast.in/" >Andrew Bastin</a>
+          </li>
+          <li class="my-2">
+            <a class="font-semibold italic underline" href="https://www.linkedin.com/in/olga-r-ibragimova/">Olga Ibragimova</a>
+          </li>
+          <li class="my-2">
+            <a class="font-semibold italic underline" href="https://www.linkedin.com/in/shen-paul/">Paul Shen</a>
+          </li>
+          <li class="my-2">
+            <a class="font-semibold italic underline" href="https://www.linkedin.com/in/stefan-ramdhan/">Stefan Ramdhan</a>
+          </li>
+        </ul>
+  </p>
+</div>
   </div>
 </template>
 <style>
